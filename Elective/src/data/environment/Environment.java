7841474739967fe362ac624ed5089579afeea537
weazen <tsen9731@outlook.com>
2017-05-14 23:50:37 +0800

    relative file path

--- Elective/src/data/environment/Environment.java
+++ Elective/src/data/environment/Environment.java
@@ -1,6 +1,6 @@
 package data.environment;
 
 public class Environment {
-	public static String studentPath = "E:/学习/课程讲义与作业/计算机双学位/软件工程/Elective/DataBase/StudentDataBase/";
-	public static String coursePath = "E:/学习/课程讲义与作业/计算机双学位/软件工程/Elective/DataBase/CourseDataBase/";
+	public static String studentPath = "DataBase/StudentDataBase/";
+	public static String coursePath = "DataBase/CourseDataBase/";
 }
